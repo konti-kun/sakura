422fb1e70f067eaf892f80c6dc31ca47cb3533ec
konti-kun <konti-kun@users.noreply.github.com>
2019-09-30 20:55:12 +0900

    一般公開用のproductの一覧をscopeで表現

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :image, presence: true
   validates :sort_key, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :published, -> { where(is_displayed: true).order(sort_key: 'DESC') }
 end
